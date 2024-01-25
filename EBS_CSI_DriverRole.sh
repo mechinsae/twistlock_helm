5d6636b3e839d65a837a3b1da7248f9661319a94
mechinsae <97924819+mechinsae@users.noreply.github.com>
2024-01-25 12:12:22 +0900

    Update EBS_CSI_DriverRole.sh
    OIDC 설치 여부 확인 후 프로세스 진행

--- EBS_CSI_DriverRole.sh
+++ EBS_CSI_DriverRole.sh
@@ -12,15 +12,23 @@ ROLE_NAME="AmazonEKS_EBS_CSI_DriverRole"
 # Ask if the user wants to install or delete the EBS CSI Driver
 read -p "Do you want to (I)nstall or (D)elete the EBS CSI Driver? Enter 'I' or 'D': " ACTION
 
-if [ "$ACTION" == "I" ] || [ "$ACTION" == "i" ]; then
+if [[ "$ACTION" == "I" || "$ACTION" == "i" ]]; then
   # Installation
 
   # Check if the EBS CSI Driver addon is already installed
   if eksctl get addon --cluster=$EKS_CLUSTER_NAME | grep -q 'aws-ebs-csi-driver'; then
     echo "EBS CSI Driver is already installed."
   else
-    # EBS CSI Driver is not installed, proceed with installation
-    eksctl utils associate-iam-oidc-provider --region=ap-northeast-2 --cluster=$EKS_CLUSTER_NAME --approve
+    # Check if open-id-connect-providers already installed
+    if aws iam list-open-id-connect-providers --query "OpenIDConnectProviderList[0].Arn" --output text | grep -q 'oidc'; then
+      #Check OIDC ARN
+      oidc_arn=$(aws iam list-open-id-connect-providers --query "OpenIDConnectProviderList[0].Arn" --output text)
+      echo "iam-oidc-provider ARN: $oidc_arn"
+      echo "open-id-connect-providers is already installed. Proceed without this step."
+    else
+      # EBS CSI Driver is not installed, proceed with installation
+      eksctl utils associate-iam-oidc-provider --region=ap-northeast-2 --cluster=$EKS_CLUSTER_NAME --approve
+    fi
 
     # Create IAM service account
     eksctl create iamserviceaccount \
@@ -36,21 +44,23 @@ if [ "$ACTION" == "I" ] || [ "$ACTION" == "i" ]; then
     # Create addon for AWS EBS CSI driver
     eksctl create addon --name aws-ebs-csi-driver --cluster $EKS_CLUSTER_NAME --service-account-role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/$ROLE_NAME --force
 
+    # Check OIDC ARN
+    oidc_arn=$(aws iam list-open-id-connect-providers --query "OpenIDConnectProviderList[0].Arn" --output text)
+
     echo "EBS CSI Driver Installation completed."
   fi
 
-elif [ "$ACTION" == "D" ] || [ "$ACTION" == "d" ]; then
+elif [[ "$ACTION" == "D" || "$ACTION" == "d" ]]; then
   # Deletion
 
   # Check if the EBS CSI Driver addon is installed
   if eksctl get addon --cluster=$EKS_CLUSTER_NAME | grep -q 'aws-ebs-csi-driver'; then
     # EBS CSI Driver is installed, proceed with deletion
 
-    # Delete addon for AWS EBS CSI driver
-    eksctl delete addon --name aws-ebs-csi-driver --cluster $EKS_CLUSTER_NAME
-
     # Delete IAM service account
     eksctl delete iamserviceaccount --cluster $EKS_CLUSTER_NAME --namespace kube-system --name ebs-csi-controller-sa
+    # Delete addon for AWS EBS CSI driver
+    eksctl delete addon --name aws-ebs-csi-driver --cluster $EKS_CLUSTER_NAME
 
     echo "EBS CSI Driver deletion completed."
   else
@@ -60,5 +70,3 @@ elif [ "$ACTION" == "D" ] || [ "$ACTION" == "d" ]; then
 else
   echo "Invalid choice. Please enter 'I' for Install or 'D' for Delete."
 fi
-
-
